8b4de4abafc5ea93392b427e0839c58a3ae36d94
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2018-07-29 01:09:05 +0200

    Bugtweaking

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-console.log("%cI see you're interested in my work. You can check out this websites code on https://github.com/sullrich84/deepsource.de. Enjoy!", "color: blue; font-size:15px;");
+$(window).resize(function() {
+  location.reload();
+});
+
+console.clear();
+console.log("I see you're interested in my work. Thank you! You can check out this websites code on https://github.com/sullrich84/deepsource.de. Enjoy!");
