69da9eb3f70f8e14b7d71a4387294ca68d8a915c
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2018-07-29 12:05:43 +0200

    Updated mobile experience

--- js/main.js
+++ js/main.js
@@ -2,5 +2,11 @@ $(window).resize(function() {
   location.reload();
 });
 
+document.addEventListener('touchmove', function(e) {
+    e.preventDefault();
+}, { passive: false });
+
+
+
 console.clear();
 console.log("I see you're interested in my work. Thank you! You can check out this websites code on https://github.com/sullrich84/deepsource.de. Enjoy!");
